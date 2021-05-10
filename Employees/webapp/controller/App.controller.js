b6843b90436d8471b5a1e968c0074bbd0f24f0cf
Giancast31st <giancast31st@gmail.com>
2021-05-10 22:10:30 +0000

    First Commit 04

--- Employees/webapp/controller/App.controller.js
+++ Employees/webapp/controller/App.controller.js
@@ -1,15 +1,32 @@
+// @ts-nocheck
+
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("chedraui.com.Employees.controller.App", {
-			onInit: function () {
+        return Controller.extend("chedraui.com.Employees.controller.App", {
+            onInit: function () {
+                this.oView = this.getView();
+            },
 
-			}
-		});
-	});
+            onValidate: function () {
+                var inputEmployee = this.oView.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                   // inputEmployee.setDescription("OK");
+                   this.oView.byId("labelCountry").setVisible(true);
+                   this.oView.byId("slCountry").setVisible(true);
+                } else {
+                   // inputEmployee.setDescription("Not OK");
+                   this.oView.byId("labelCountry").setVisible(false);
+                   this.oView.byId("slCountry").setVisible(false);
+                }
+            }
+        });
+    });
